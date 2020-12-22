e1bdcea9d70ca38a51b4c51c41b1360c91541dd7
Amritpreet Singh <amritpreet98@gmail.com>
2020-12-22 16:14:44 -0600

    siwtched to theAlgorithm's non-recursive merge_sort implementation

--- algorithms_c/sorting/scriptsWithFileReading/merge.c
+++ algorithms_c/sorting/scriptsWithFileReading/merge.c
@@ -1,37 +1,48 @@
-/* Source: https://github.com/caisah/Sedgewick-algorithms-in-c-exercises-and-examples/blob/master/08-merging_and_mergesort/8.03-top_down_mergesort/examples/prog_8.3-top_down_mergesort.c */
-#include <stdio.h> 
+/* Reference: https://github.com/TheAlgorithms/C/blob/master/sorting/merge_sort_nr.c */
+#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "merge.h"
 
-#define key(A) (A)
-#define less(A, B) (key(A) < key(B))
-
-typedef long Item;
-
-long *aux;
-
-void merge(Item a[], int l, int m, int r)
+void merge_sort(long x[], int n)
 {
-  int i, j, k;
+    long* temp = (long *) calloc(n, sizeof(long));
+    int i, j, k, lb1, lb2, ub1, ub2, size;
 
-  for (i = m+1; i > l; i--)
-    aux[i-1] = a[i-1];
-  for (j = m; j < r; j++)
-    aux[r+m-j] = a[j+1];
-  for (k = l; k <= r; k++)
-    if (less(aux[j], aux[i]))
-      a[k] = aux[j--];
-    else
-      a[k] = aux[i++];
-}
+    size = 1;
+    while (size < n)
+    {
+        lb1 = 0;
+        k = 0;
 
-void merge_sort(Item a[], int l, int r)
-{
-  int m = (r+l)/2;
+        while (lb1 + size < n)
+        {
+            lb2 = lb1 + size;
+            ub1 = lb2 - 1;
+            if (ub1 + size < n)
+                ub2 = ub1 + size;
+            else
+                ub2 = n - 1;
 
-  if (r <= l) return;
-  merge_sort(a, l, m);
-  merge_sort(a, m+1, r);
-  merge(a, l, m, r);
+            i = lb1;
+            j = lb2;
+
+            while (i <= ub1 && j <= ub2)
+                if (x[i] < x[j])
+                    temp[k++] = x[i++];
+                else
+                    temp[k++] = x[j++];
+
+            while (i <= ub1) temp[k++] = x[i++];
+
+            while (j <= ub2) temp[k++] = x[j++];
+
+            lb1 = ub2 + 1;
+        }
+
+        for (i = 0; i <= ub2; i++) x[i] = temp[i];
+
+        size = size * 2;
+    }
+
+    free(temp);
 }
--- algorithms_c/sorting/scriptsWithFileReading/merge.h
+++ algorithms_c/sorting/scriptsWithFileReading/merge.h
@@ -1,8 +1,5 @@
-/* Source: https://github.com/caisah/Sedgewick-algorithms-in-c-exercises-and-examples/blob/master/08-merging_and_mergesort/8.03-top_down_mergesort/examples/prog_8.3-top_down_mergesort.c */
+/* Source: https://github.com/TheAlgorithms/C/blob/master/sorting/merge_sort_nr.c */
 #ifndef MERGE_H
 #define MERGE_H
-typedef long Item;
-
-void merge(Item a[], int l, int m, int r);
-void merge_sort(Item a[], int l, int r);
+void merge_sort(long x[], int n);
 #endif
